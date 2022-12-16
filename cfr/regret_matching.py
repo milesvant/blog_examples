326ae0e5f54f2616c988a8632aa3d09c84d340bf
Miles Van Tongeren <mnv2108@columbia.edu>
2022-12-16 15:52:48 -0800

    Add Prisoner's Dilemma and fix rm bug

--- cfr/regret_matching.py
+++ cfr/regret_matching.py
@@ -1,26 +1,31 @@
 import numpy as np
 
 
-class RPS:
+class Game:
+    def play(self, identity_hero, action_hero, action_villan):
+        return self.payoffs[action_hero][action_villan]
+
+
+class RPS(Game):
     def __init__(self):
         self.name = "Rock-Paper-Scissors"
-        self.n_actions = [3, 3]
-        self.R = 0
-        self.P = 1
-        self.S = 2
+        self.payoffs = [[0, -1, 1], [1, 0, -1], [-1, 1, 0]]
+        self.n_actions = (3, 3)
 
-    def play(self, identity_hero, action_hero, action_villan):
-        if action_hero == action_villan:
-            return 0
-        if action_hero == self.R:
-            return 1 if action_villan == self.S else -1
-        if action_hero == self.S:
-            return 1 if action_villan == self.P else -1
-        if action_hero == self.P:
-            return 1 if action_villan == self.R else -1
 
     def strategy_string(self, strategy):
-        return f"Rock: { strategy[self.R] }, Paper: { strategy[self.P] }, Scissors: { strategy[self.S] }"
+        return f"Rock: { strategy[0] }, Paper: { strategy[1] }, Scissors: { strategy[2] }"
+
+
+class PrisonersDilemma(Game):
+    def __init__(self):
+        self.name = "Prisoner's Dilemma"
+        self.payoffs = [[-5, 0], [-10, -2]]
+        self.n_actions = (2, 2)
+
+
+    def strategy_string(self, strategy):
+        return f"Defect: { strategy[0] }, Cooperate: {strategy[1] }"
 
 
 
@@ -43,7 +48,8 @@ class RegretMatching:
         regrets[regrets < 0] = 0
         total_pos_regret = regrets.sum()
         if total_pos_regret == 0:
-            return np.uniform(regrets.size)
+            strategy = np.random.uniform(size=regrets.size)
+            return strategy / strategy.sum()
         else:
             return regrets / total_pos_regret
 
@@ -55,20 +61,27 @@ class RegretMatching:
         strategy2 = np.random.random(self.game.n_actions[1])
         strategy2 /= strategy2.sum()
 
+        cum_regrets1, cum_regrets2 = np.zeros(strategy1.size), np.zeros(strategy2.size)
         cum_strategy1, cum_strategy2 = strategy1, strategy2
 
         for _ in range(iterations):
-            strategy1 = self.get_strategy(self.calculate_regrets(0, strategy1, strategy2))
-            strategy2 = self.get_strategy(self.calculate_regrets(1, strategy2, strategy1))
-
+            cum_regrets1 += self.calculate_regrets(0, strategy1, strategy2)
+            cum_regrets2 += self.calculate_regrets(1, strategy2, strategy1)
+            strategy1 = self.get_strategy(cum_regrets1)
+            strategy2 = self.get_strategy(cum_regrets2)
             cum_strategy1 += strategy1
             cum_strategy2 += strategy2
 
-        nash1, nash2 = cum_strategy1 / iterations, cum_strategy2 / iterations
+        nash1, nash2 = cum_strategy1 / (iterations + 1), cum_strategy2 / (iterations + 1)
         print(f"After { iterations } iterations,  player 1 plays [{ self.game.strategy_string(nash1) }] and player 2 plays [{ self.game.strategy_string(nash2) }]")
         return nash1, nash2
 
 
 if __name__ == "__main__":
-    rm = RegretMatching(RPS())
-    strategy1, strategy2 = rm.solve(1000)
+    print("__________________________________________________________________________________________________")
+    rps_rm = RegretMatching(RPS())
+    rps_rm.solve(1000)
+    print("__________________________________________________________________________________________________")
+    pd_rm = RegretMatching(PrisonersDilemma())
+    pd_rm.solve(1000)
+    print("__________________________________________________________________________________________________")
